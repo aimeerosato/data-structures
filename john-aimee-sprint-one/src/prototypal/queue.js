57340406c9886e10605ea99ca0735fa5e29d0db9
Aimee Rosato <aimee.rosato@gmail.com>
2016-01-14 14:55:04 -0500

    completed prototypal and pseudoclassical

--- john-aimee-sprint-one/src/prototypal/queue.js
+++ john-aimee-sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(Queue.prototype);
+
+  someInstance.count = 0;
+  someInstance.removed = 0;
+  someInstance.storage = {};
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
-
+Queue.prototype.size = function(){
+    return this.count - this.removed;
+};
+Queue.prototype.enqueue = function(value){
+    this.storage[this.count] = value; 
+    this.count++;
+};
+Queue.prototype.dequeue = function(){
+  if(this.count-this.removed){
+    var result = this.storage[this.removed];
+    this.removed++;
+    return result;
+  }
+};
 
--- john-aimee-sprint-one/src/prototypal/stack.js
+++ john-aimee-sprint-one/src/prototypal/stack.js
@@ -1,23 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  /*var properties = {
-    count: 0,
-    storage: {}
-  };
+  var stackInstance = Object.create(Stack.prototype);
   
-  var stackInstance = Object.create(properties);
-  */
+  stackInstance.count = 0; 
+  stackInstance.storage = {};
+  
+  return stackInstance;
 };
-  
-  Stack.prototype.size = function() { return this.count; };
-  Stack.prototype.push = function(value) {
-    this.storage[count] = value;
+   //  Had to add in constructor 
+   //  Wanted to use extend, but couldn't access library
+Stack.prototype = {
+  constructor: Stack,
+  size : function() { return this.count; },
+  push : function(value) {
+    this.storage[this.count] = value;
     this.count++;
-  };
-  Stack.prototype.pop = function() {
+  },
+  pop : function() {
     if (this.count) {
+      this.count--;
       return this.storage[this.count];
     }
-  };
+  }
+};
+  
+  // Works
+  // Stack.prototype.size = function() { return this.count; };
+  // Stack.prototype.push = function(value) {
+  //   this.storage[this.count] = value;
+  //   this.count++;
+  // };
+  // Stack.prototype.pop = function() {
+  //   if (this.count) {
+  //     this.count--;
+  //     return this.storage[this.count];
+  //   }
+  // };
 
