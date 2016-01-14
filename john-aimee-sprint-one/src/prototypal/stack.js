535365b319ae6b07031c0d4d087f2008804526af
Aimee Rosato <aimee.rosato@gmail.com>
2016-01-13 22:44:08 -0500

    worked on queue prototypal

--- john-aimee-sprint-one/src/prototypal/stack.js
+++ john-aimee-sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  /*var properties = {
+    count: 0,
+    storage: {}
+  };
+  
+  var stackInstance = Object.create(properties);
+  */
 };
-
-var stackMethods = {};
-
+  
+  Stack.prototype.size = function() { return this.count; };
+  Stack.prototype.push = function(value) {
+    this.storage[count] = value;
+    this.count++;
+  };
+  Stack.prototype.pop = function() {
+    if (this.count) {
+      return this.storage[this.count];
+    }
+  };
 
