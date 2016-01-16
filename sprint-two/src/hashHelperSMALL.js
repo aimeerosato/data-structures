c2755f2a669961779194a470547e009814b8ada4
Aimee Rosato <aimee.rosato@gmail.com>
2016-01-15 22:24:43 -0500

    hashtable complete

---
+++ sprint-two/src/hashHelperSMALL.js
@@ -0,0 +1,12 @@
+ limitedArray.get = function(index){
+    checkLimit(index);
+    return storage[index];
+  };
+  limitedArray.set = function(index, value){
+    checkLimit(index);
+    storage[index] = value;
+  };
+  limitedArray.each = function(callback){
+    for(var i = 0; i < storage.length; i++){
+      callback(storage[i], i, storage);
+    }
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,23 +1,87 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  //storage = [bucket1, bucket2, bucket3, ...] bucket[i]=[keyval1, keyval2, ...] keyval[i]=[key, value]
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  // Create key/value pair in a 2-tuple
+  var bucket;
+  var keyValPair = LimitedArray(2);
+  keyValPair.set(0, key);
+  keyValPair.set(1, value); 
+  
+  // If there is a bucket at index, put key/val at second index. (else at first)  
+  if(this._storage.get(i)){
+    bucket = this._storage.get(i);
+    var keyVal1 = bucket.get(0);
+    var keyVal2 = bucket.get(1);
+    
+    if(keyVal1){
+      if (key === bucket.get(0).get(0)) {
+        bucket.set(0, keyValPair);  
+      } 
+    } 
+    if(keyVal2) {
+      if (key === bucket.get(1).get(0)) {
+        bucket.set(1, keyValPair);
+      } else {
+        bucket.set(0, keyValPair);
+      }
+    }
+    
+    bucket.set(1, keyValPair);
+    
+  } else {
+    bucket = LimitedArray(2);
+    bucket.set(0, keyValPair);
+    this._storage.set(i, bucket);
+  }
 };
 
-HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+HashTable.prototype.retrieve = function(key){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  //look in bucket at first index, if that is i, return...if return what's in position 2
+  var bucket = this._storage.get(i);
+  if(bucket){
+    var keyVal1 = bucket.get(0);
+    var keyVal2 = bucket.get(1);
+    
+    if(keyVal1){
+      if(keyVal1.get(0) === key) { 
+        return keyVal1.get(1); 
+      } 
+    }
+    if(keyVal2){
+      if(keyVal2.get(0) === key){
+        return keyVal2.get(1);
+      }
+    } 
+  }
+  return null;
 };
 
-HashTable.prototype.remove = function(k){
+HashTable.prototype.remove = function(key){
+ var i = getIndexBelowMaxForKey(key, this._limit);
+ var bucket = this._storage.get(i);
+ 
+ if (bucket) {
+  if (key === bucket.get(0).get(0)) {
+    bucket.get(0).set(0, null);
+    bucket.get(0).set(1, null);
+  } else if (key === bucket.get(1).get(0)) {
+    bucket.get(1).set(0, null);
+    bucket.get(1).set(1, null);
+  }
+ }
+
 
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert() = O(1)
+ retrieve() = O(1)
+ remove() = O(1)
  */
